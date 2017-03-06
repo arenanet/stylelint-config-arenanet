82f40d4153432e996fdc66807c11f0dd95557984
Pat Cavit <github@patcavit.com>
2017-03-06 14:39:20 -0800

    fix: change rule-empty-line-before & number-no-trailing-zeros
    Also alphabetized rules

--- index.js
+++ index.js
@@ -3,50 +3,35 @@
 module.exports = {
     "extends": "stylelint-config-standard",
     "rules": {
+        "at-rule-empty-line-before": [ "always", {
+            "except": "first-nested",
+            "ignore": "blockless-after-blockless"
+        } ],
         "color-hex-case": "upper",
-        "indentation": 4,
-        "declaration-empty-line-before": null,
-        "at-rule-empty-line-before": [
-            "always",
-            {
-                "except": "first-nested",
-                "ignore": "blockless-after-blockless"
-            }
-        ],
-        "property-no-unknown": [
-            true,
-            {
-                "ignoreProperties": [
-                    "composes"
-                ]
-            }
-        ],
         "comment-empty-line-before": null,
-        "no-eol-whitespace": [
-            true,
-            {
-                "ignore": "empty-lines"
-            }
-        ],
-        "rule-empty-line-before": [
-            "always",
-            {
-                "ignore": [
-                    "after-comment",
-                    "inside-block"
-                ],
-                "except": [
-                    "first-nested"
-                ]
-            }
-        ],
-        "selector-pseudo-class-no-unknown": [
-            true,
-            {
-                "ignorePseudoClasses": [
-                    "global"
-                ]
-            }
-        ]
+        "declaration-empty-line-before": null,
+        "indentation": 4,
+        "no-eol-whitespace": [ true, {
+            "ignore": "empty-lines"
+        }],
+        "number-no-trailing-zeros": null,
+        "property-no-unknown": [ true, {
+            "ignoreProperties": [
+                "composes"
+            ]
+        } ],
+        "rule-empty-line-before": [ "always-multi-line", {
+            "ignore": [
+                "after-comment"
+            ],
+            "except": [
+                "first-nested"
+            ]
+        } ],
+        "selector-pseudo-class-no-unknown": [ true, {
+            "ignorePseudoClasses": [
+                "global"
+            ]
+        } ]
     }
 };
